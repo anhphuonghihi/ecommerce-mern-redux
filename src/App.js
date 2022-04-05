6d4ae713695ac1b5a3d8d598b9e1ae811eddafd7
Phuong Ho Le <lehoangyen172401@gmail.com>
2022-04-05 22:19:07 +0700

    Product List Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { useState } from "react";
+import data from "./data.json";
+import Products from "./components/Products";
+const App = () => {
+  const [products, setProducts] = useState(data.products);
+  console.log("🚀 ~ file: App.js ~ line 6 ~ App ~ products", products)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid-container">
+      <header>
+        <a href="/">React Shopping Cart</a>
       </header>
+      <main>
+        <div className="content">
+          <div className="main">
+            <Products products={products}></Products>
+          </div>
+          <div className="sidebar">Cart Items</div>
+        </div>
+      </main>
+      <footer>All right is reserved.</footer>
     </div>
   );
-}
+};
 
 export default App;
