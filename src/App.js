2010c2c1c24bf26bcaa4dc60d152e5561f98bc47
Phuong Ho Le <lehoangyen172401@gmail.com>
2022-04-07 10:36:11 +0700

    Filter Bar Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
 import { useState } from "react";
 import data from "./data.json";
 import Products from "./components/Products";
+import Filter from "./components/Filter";
 const App = () => {
   const [products, setProducts] = useState(data.products);
-  console.log("🚀 ~ file: App.js ~ line 6 ~ App ~ products", products)
+  const [size, setSize] = useState("");
+  const [sort, setSort] = useState("");
+  const filterProducts = (event) => {
+    const sort = event.target.value;
+    console.log(event.target.value);
+    setSort(sort);
+    setProducts(
+      products
+        .slice()
+        .sort((a, b) =>
+          sort === "lowest"
+            ? a.price > b.price
+              ? 1
+              : -1
+            : sort === "highest"
+            ? a.price < b.price
+              ? 1
+              : -1
+            : a._id < b._id
+            ? 1
+            : -1
+        )
+    );
+  };
+  const sortProducts = (event) => {
+    console.log(event.target.value);
+    if (event.target.value === "") {
+      setSize(event.target.value);
+      setProducts(data.products);
+    } else {
+      setSize(event.target.value);
+      setProducts(
+        data.products.filter(
+          (product) => product.availableSizes.indexOf(event.target.value) >= 0
+        )
+      );
+    }
+  };
   return (
     <div className="grid-container">
       <header>
@@ -12,6 +50,13 @@ const App = () => {
       <main>
         <div className="content">
           <div className="main">
+            <Filter
+              count={products.length}
+              size={size}
+              sort={sort}
+              filterProducts={filterProducts}
+              sortProducts={sortProducts}
+            ></Filter>
             <Products products={products}></Products>
           </div>
           <div className="sidebar">Cart Items</div>
