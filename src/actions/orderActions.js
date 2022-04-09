c7e8f6af648de9b465a88452c51ade08be1b2ba1
Phuong Ho Le <lehoangyen172401@gmail.com>
2022-04-09 15:15:20 +0700

    14-Fix Bug local

--- src/actions/orderActions.js
+++ src/actions/orderActions.js
@@ -1,7 +1,7 @@
 import { CREATE_ORDER, CLEAR_CART, CLEAR_ORDER } from "../types";
 import { FETCH_ORDERS } from "../types";
 export const createOrder = (order) => (dispatch) => {
-  fetch("http://localhost:5000/api/orders", {
+  fetch("/api/orders", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -19,7 +19,7 @@ export const clearOrder = () => (dispatch) => {
   dispatch({ type: CLEAR_ORDER });
 };
 export const fetchOrders = () => (dispatch) => {
-  fetch("http://localhost:5000/api/orders")
+  fetch("/api/orders")
     .then((res) => res.json())
     .then((data) => {
       dispatch({ type: FETCH_ORDERS, payload: data });
