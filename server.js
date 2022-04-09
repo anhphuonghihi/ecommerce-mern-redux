15a6b6948c46406bae92c132d08b82c38b0d0504
Phuong Ho Le <lehoangyen172401@gmail.com>
2022-04-09 08:55:28 +0700

    Create Order

--- server.js
+++ server.js
@@ -61,3 +61,43 @@ app.delete("/api/products/:id", async (req, res) => {
     return res.status(500).json({ message: error.message });
   }
 });
+
+const CartSchema = new mongoose.Schema(
+  {
+    email: String,
+    name: String,
+    address: String,
+    total: Number,
+    cartItems: [
+      {
+        _id: String,
+        title: String,
+        price: Number,
+        count: Number,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Order = mongoose.model("cart", CartSchema);
+
+app.post("/api/orders", async (req, res) => {
+  try {
+    if (
+      !req.body.name ||
+      !req.body.email ||
+      !req.body.address ||
+      !req.body.total ||
+      !req.body.cartItems
+    ) {
+      return res.json({ message: "Data is required." });
+    }
+    const order = await Order(req.body).save();
+    res.json(order);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+});
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,8 +2,17 @@ import { useState } from "react";
 import formatCurrency from "../formatCurrency";
 import Fade from "react-reveal/Fade";
 import { connect } from "react-redux";
+import Modal from "react-modal";
+import Zoom from "react-reveal/Zoom";
+import { createOrder, clearOrder } from "../actions/orderActions";
 import { removeFromCart } from "../actions/cartActions";
-const Cart = ({ cartItems, removeFromCart, createOrder }) => {
+const Cart = ({
+  createOrder,
+  clearOrder,
+  cartItems,
+  order,
+  removeFromCart,
+}) => {
   const initState = {
     name: "",
     email: "",
@@ -15,16 +24,19 @@ const Cart = ({ cartItems, removeFromCart, createOrder }) => {
     const { name, value } = e.target;
     setUser({ ...user, [name]: value });
   };
-  const onSubmitOrder = (e) => {
+  const createOrderSubmit = (e) => {
     e.preventDefault();
     const order = {
       name: user.name,
       email: user.email,
       address: user.address,
-      cartItems: user.cartItems,
+      cartItems: cartItems,
+      total: cartItems.reduce((a, c) => a + c.price * c.count, 0),
     };
     createOrder(order);
-    setUser(initState);
+  };
+  const closeModal = () => {
+    clearOrder();
   };
   return (
     <div>
@@ -34,6 +46,52 @@ const Cart = ({ cartItems, removeFromCart, createOrder }) => {
         <div className="cart cart-header">
           You have {cartItems.length} in the cart
         </div>
+      )}
+
+      {order && (
+        <Modal isOpen={true} onRequestClose={closeModal}>
+          <Zoom>
+            <button className="close-modal" onClick={closeModal}>
+              x
+            </button>
+            <div className="order-details">
+              <h3 className="success-message">Your order has been placed.</h3>
+              <h2>Order {order._id}</h2>
+              <ul>
+                <li>
+                  <div>Name:</div>
+                  <div>{order.name}</div>
+                </li>
+                <li>
+                  <div>Email:</div>
+                  <div>{order.email}</div>
+                </li>
+                <li>
+                  <div>Address:</div>
+                  <div>{order.address}</div>
+                </li>
+                <li>
+                  <div>Date:</div>
+                  <div>{order.createdAt}</div>
+                </li>
+                <li>
+                  <div>Total:</div>
+                  <div>{formatCurrency(order.total)}</div>
+                </li>
+                <li>
+                  <div>Cart Items:</div>
+                  <div>
+                    {order.cartItems.map((x) => (
+                      <div>
+                        {x.count} {" x "} {x.title}
+                      </div>
+                    ))}
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </Zoom>
+        </Modal>
       )}
       <div>
         <div className="cart">
@@ -63,62 +121,64 @@ const Cart = ({ cartItems, removeFromCart, createOrder }) => {
         </div>
         {cartItems.length !== 0 && (
           <Fade right cascade>
-            <div className="cart">
-              <div className="total">
-                <div>
-                  Total:
-                  {formatCurrency(
-                    cartItems.reduce((a, c) => a + c.price * c.count, 0)
-                  )}
-                </div>
-                <button
-                  onClick={() => {
-                    setUser({ ...user, showCheckout: true });
-                  }}
-                  className="button primary"
-                >
-                  Proceed
-                </button>
+              <div className="cart">
+                <div className="total">
+                  <div>
+                    Total:
+                    {formatCurrency(
+                      cartItems.reduce((a, c) => a + c.price * c.count, 0)
+                    )}
+                  </div>
+                  <button
+                    onClick={() => {
+                      setUser({ ...user, showCheckout: true });
+                    }}
+                    className="button primary"
+                  >
+                    Proceed
+                  </button>      
               </div>
               {user.showCheckout && (
-                <div className="cart">
-                  <form onSubmit={onSubmitOrder}>
-                    <ul className="form-container">
-                      <li>
-                        <label>Email</label>
-                        <input
-                          name="email"
-                          type="email"
-                          required
-                          onChange={handleInput}
-                        ></input>
-                      </li>
-                      <li>
-                        <label>Name</label>
-                        <input
-                          name="name"
-                          type="text"
-                          required
-                          onChange={handleInput}
-                        ></input>
-                      </li>
-                      <li>
-                        <label>Address</label>
-                        <input
-                          name="address"
-                          type="text"
-                          required
-                          onChange={handleInput}
-                        ></input>
-                      </li>
-                      <li>
-                        <button className="button primary" type="submit">
-                          Checkout
-                        </button>
-                      </li>
-                    </ul>
-                  </form>
-                </div>
+                <Fade right cascade>
+                  <div className="cart">
+                    <form onSubmit={createOrderSubmit}>
+                      <ul className="form-container">
+                        <li>
+                          <label>Email</label>
+                          <input
+                            name="email"
+                            type="email"
+                            required
+                            onChange={handleInput}
+                          ></input>
+                        </li>
+                        <li>
+                          <label>Name</label>
+                          <input
+                            name="name"
+                            type="text"
+                            required
+                            onChange={handleInput}
+                          ></input>
+                        </li>
+                        <li>
+                          <label>Address</label>
+                          <input
+                            name="address"
+                            type="text"
+                            required
+                            onChange={handleInput}
+                          ></input>
+                        </li>
+                        <li>
+                          <button className="button primary" type="submit">
+                            Checkout
+                          </button>
+                        </li>
+                      </ul>
+                    </form>
+                  </div>
+                </Fade>
               )}
             </div>
           </Fade>
@@ -127,10 +187,10 @@ const Cart = ({ cartItems, removeFromCart, createOrder }) => {
     </div>
   );
 };
-
 export default connect(
   (state) => ({
+    order: state.order.order,
     cartItems: state.cart.cartItems,
   }),
-  { removeFromCart }
+  { removeFromCart, createOrder, clearOrder }
 )(Cart);
